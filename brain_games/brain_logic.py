8d2502becd891d1a43867ebce308ee6eac0a3c10
Prikhodko <admin@wiland.ru>
2022-07-04 12:35:11 +0300

    a file with a description of the logic of the game

---
+++ brain_games/brain_logic.py
@@ -0,0 +1,25 @@
+from random import randint
+import prompt
+
+
+def game_logic():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!\nAnswer "yes" if the number is even, otherwise answer "no".')
+    i = 0
+
+    while i <= 2:
+        random_number = randint(1, 20)
+        print(f'Question: {random_number}')
+        answer = prompt.string('Your answer: ')
+        if random_number % 2 == 0 and answer != 'yes':
+            print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes'. \nLet's try again, {name}!")
+            break
+        elif random_number % 2 != 0 and answer != 'no':
+            print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'. \nLet's try again, {name}!")
+            break
+        else:
+            print('Correct!')
+            i += 1
+        if i > 2:
+            print(f'Congratulations, {name}!')
