930ebc910e4fd74e927e1244d8b110f82348d61b
Prikhodko <admin@wiland.ru>
2022-07-11 18:06:27 +0300

    error correction

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,8 +3,7 @@ from brain_games.cli import welcome_user
 
 
 def main():
-    print('Welcome to the Brain Games!')
-    user_name = welcome_user()
+    welcome_user()
 
 
 if __name__ == '__main__':
